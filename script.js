3c984728e6f3ba6d8af3458d4673d70937b3d3b8
Michaelhybrid <michaeloludare15@gmail.com>
2022-09-19 00:22:27 +0100

    night

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-let cardHolderNameInput = document.querySelector('cardholder-name');
-console.log(cardHolderNameInput.value)
+//!INITIALIZATION OF VARIABLES
+let cardHolderNameInput = document.querySelector('#cardholder-name');
+let cardHolderNameCard  = document.querySelector('.cardholder-name-card');
+let cardNumberInput = document.querySelector('#card-number');
+let cardNumberCard = document.querySelector('.card-number-card');
+
+//?CARDHOLDER NAME
+cardHolderNameInput.addEventListener('keyup',()=>{
+    cardHolderNameCard.innerHTML = cardHolderNameInput.value;
+});
+
+//?CARD NUMBER
+cardNumberInput.addEventListener('keyup',()=>{
+    cardNumberCard.innerHTML = cardNumberInput.value;
+});
